a9554022a4032b0fe6cee7ab20b14fef54ceef24
vasilev25 <pvdev95@gmail.com>
2017-03-19 16:35:18 +0200

    new DB update

---
+++ RealEstate/RealEstate/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RealEstate.Controllers
+{
+    public class UserController : Controller
+    {
+        realestatesystemEntities db = new realestatesystemEntities();
+        public ActionResult Index ()
+        {
+            return View(db.USERS.ToList());
+        }
+        
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create (USER newUser)
+        {
+            if (ModelState.IsValid)
+            {
+                db.USERS.Add(newUser);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(newUser);
+        }
+    }
+}
